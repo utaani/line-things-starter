4e665f0295c2e9abc77673d0ba139559713f0ea7
Takeshi Ueno <utaani@ueno.org>
2019-02-02 16:07:07 +0900

    update uuid

--- liff-app/liff.js
+++ liff-app/liff.js
@@ -1,5 +1,5 @@
 // User service UUID: Change this to your generated service UUID
-const USER_SERVICE_UUID         = '094a3bb2-8e9c-4662-b4c3-1fb378dd082a'; // LED, Button
+const USER_SERVICE_UUID         = '0ce7184d-5109-4d91-b304-9ebd0cffd506'; // LED, Button
 // User service characteristics
 const LED_CHARACTERISTIC_UUID   = 'E9062E71-9E62-4BC6-B0D3-35CDCD9B027B';
 const BTN_CHARACTERISTIC_UUID   = '62FBD229-6EDD-4D1A-B554-5C4E1BB29169';
